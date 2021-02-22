dd97541b0bc324ffe381e960e459463755aa9c27
Emanuel Filipe <efilipecode@gmail.com>
2021-02-22 17:11:44 -0300

    Add image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,44 @@
 <script>
+	import studying from "images/studying.svg";
 </script>
 
 <svelte:head>
 	<title>Greek Project</title>
 </svelte:head>
 
+<main id="initial-screen">
+	<div>
+		<h1>Greek Project</h1>
+		<p><strong>A little project for greek learners.</strong></p>
+		<a href="/select">
+			<button>Get Started!</button>
+		</a>
+	</div>
+
+	<figure>
+		<img alt="Studying greek" src={studying} />
+	</figure>
+
+	<p class="greek">
+		"Σοφίαν ζήλου."
+	</p>
+</main>
+
 <style>
 	#initial-screen {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
+		justify-content: space-around;
 		align-items: center;
-		font-family: 'Merriweather', serif;
 		height: 100vh;
+	}
+
+	#initial-screen div {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: center;
+		width: 100%;
 	}
 
 	#initial-screen h1 {
@@ -24,31 +50,29 @@
 	#initial-screen p {
 		color: #ddd;
 		font-size: 1em;
-		margin: 10px;
+		margin: 10px 0px 40px 0px;
 	}
 
 	#initial-screen button {
 		width: 150px;
 		height: 50px;
-		color: white;
-		font-family: inherit;
-		background-color: #6c2cec;
-		border: none;
-		outline: none;
-		margin: 10px 0px;
-		border-radius: 10px;
-		font-size: 1em;
-		cursor: pointer;
-		transition: all .2s;
+		margin: 0px 0px;
 	}
 
-	#initial-screen button:hover {
-		background-color: #341f97;
+	#initial-screen figure {
+		overflow: hidden;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	#initial-screen figure img {
+		width: 60%;
+	}
+
+	#initial-screen p.greek {
+		color: white;
+		font-family: 2em;
 	}
 </style>
-
-<main id="initial-screen">
-	<h1>Greek Project</h1>
-	<p><strong>A little project for greek learners.</strong></p>
-	<button>Get Started!</button>
-</main>
