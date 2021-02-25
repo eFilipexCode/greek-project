bf4728d7de9fa6036e4a1b22ec0dfbda80a4b6f9
Emanuel Filipe <efilipecode@gmail.com>
2021-02-25 11:41:54 -0300

    Add button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let linkUrl;
+    export let text;
+    export let className;
+    export let style;
+</script>
+
+<button>
+    {#if linkUrl}
+        <a href={linkUrl}>
+            <button {style} class={className}>
+                {text}
+            </button>
+        </a>
+    {:else}
+        <button {style} class={className}>
+            {text}
+        </button>
+    {/if}
+</button>
