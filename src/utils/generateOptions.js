116f9ce8f17ccb0df2ce0aee4dca4557f6884143
Emanuel Filipe <efilipecode@gmail.com>
2021-02-24 23:15:16 -0300

    Randomness improved

--- src/utils/generateOptions.js
+++ src/utils/generateOptions.js
@@ -1,25 +1,31 @@
+
 import selectRandomIndex from './selectRandomIndex';
 import shuffle from '../utils/arrayShuffle';
 
 export default function generateOptions(answersArray, recentSubject) {
     const newOptions = [];
+    const answersKeeper = [...answersArray];
+    const shuffledArray = shuffle(answersArray);
     for (let i = 0; i < 3; i++) {
-        const randomIndexOption = selectRandomIndex(answersArray);
+        let randomIndexOption = selectRandomIndex(shuffledArray);
         //Check if there's already another option with the same data.
-        for (let index = 0; index < newOptions.length; index++) {
-            if (newOptions[randomIndexOption] === answersArray[randomIndexOption]) {
-                i--;
+        for (let index = 0; index < newOptions.length - 1; index++) {
+            if (newOptions[randomIndexOption] === shuffledArray[randomIndexOption]) {
+                randomIndexOption++;
             };
         }
 
         if (randomIndexOption === recentSubject) {
             i--;
         } else {
-            newOptions.push(answersArray[randomIndexOption]);
+            newOptions.push(shuffledArray[randomIndexOption]);
         };
     };
 
-    newOptions.push(answersArray[recentSubject]);
+    const correct = shuffledArray.filter((item) => {
+        return item === answersKeeper[recentSubject];
+    })[0];
+    newOptions.push(correct);
     shuffle(newOptions);
     return newOptions;
 };
