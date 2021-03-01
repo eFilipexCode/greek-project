82aee29789ab4c0b697736a21633eb563693123e
Emanuel Filipe <efilipecode@gmail.com>
2021-03-01 14:42:18 -0300

    Change image path

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,11 +1,10 @@
 <script>
     import Button from '../components/Button.svelte';
-    import aristotle from '../images/aristotle.png';
 </script>
 
 <div class="error-container">
     <h1><span class="greek">οἴμοι!</span> Looks like something went wrong.</h1>
-    <img class="animate__animated animate__pulse" src={aristotle} alt="Aristotle"/>
+    <img class="animate__animated animate__pulse" src='/aristotle.png' alt="Aristotle"/>
     <Button text="Return Home" linkUrl="/" style={`width: 150px; height: 50px; margin:0`}/>
 </div>
 
