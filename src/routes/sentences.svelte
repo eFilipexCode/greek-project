1c72a00cae89fb894e3410e0d9a03192f16216f8
Emanuel Filipe <efilipecode@gmail.com>
2021-03-04 13:33:42 -0300

    Sentences page and its logic

--- src/routes/sentences.svelte
+++ src/routes/sentences.svelte
@@ -1,9 +1,119 @@
 <script>
-    import NotAvailableYet from '../components/NotAvailableYet.svelte';
+    import buildSentence from "../utils/buildSentences";
+    import {
+        subject,
+        accustive,
+        verbs,
+        predicative,
+    } from "../utils/wordsForSentences";
+    import shuffle from "../utils/arrayShuffle";
+    import playSound from "../utils/playCorrectSound";
+    import Button from "../components/Button.svelte";
+
+    let { sentence, traduction } = buildSentence(
+        subject,
+        accustive,
+        verbs,
+        predicative
+    );
+    let incorrectTranslations;
+    let options;
+
+    function generateNewSentences() {
+        incorrectTranslations = [];
+        for (let i = 0; i < 3; i++) {
+            const newIncorrectTranslation = buildSentence(
+                subject,
+                accustive,
+                verbs,
+                predicative
+            );
+            if (newIncorrectTranslation.traduction === traduction) i--;
+            else incorrectTranslations.push(newIncorrectTranslation);
+        }
+        options = [...incorrectTranslations, { sentence, traduction }];
+        shuffle(options);
+    }
+
+    generateNewSentences();
+
+    let colorAnswer = "#5f27cd";
+    function changeBg(correct) {
+        correct ? (colorAnswer = "#2ecc71") : (colorAnswer = "#e74c3c");
+        setTimeout(() => {
+            colorAnswer = "#5f27cd";
+        }, 500);
+    }
+
+    function checkAnswer(option) {
+        if (option.traduction === traduction) {
+            playSound(true);
+
+            const newMainSentence = buildSentence(
+                subject,
+                accustive,
+                verbs,
+                predicative
+            );
+            sentence = newMainSentence.sentence;
+            traduction = newMainSentence.traduction;
+
+            generateNewSentences();
+            changeBg(true);
+        } else {
+            playSound(false);
+            changeBg(false);
+        }
+    }
 </script>
 
 <svelte:head>
     <title>Greek Project - Sentences</title>
 </svelte:head>
 
-<NotAvailableYet />
+<main class="container-game" style={`background: ${colorAnswer}`}>
+    <Button
+        className="back-button"
+        linkUrl="/select"
+        text="Back"
+    />
+    <div class="options-screen">
+        <p class="greek option-show">
+            {sentence}
+        </p>
+    </div>
+    <p class="command">Means...</p>
+    <div class="options-letters">
+        {#each options as option}
+            <button
+                on:click={() => checkAnswer(option)}
+                class="greek-option option animate__animated animate__fadeInUp"
+                >{option.traduction}</button
+            >
+        {/each}
+    </div>
+</main>
+
+<style>
+    main {
+        height: 100vh;
+        width: 100vw;
+    }
+
+    .option-show {
+        font-size: 2.5rem;
+    }
+    .options-letters button {
+        font-size: 1.2rem;
+        padding: 6px;
+        min-height: 90px;
+        width: 170px;
+    }
+
+    @media (max-width: 600px) {
+        div.options-letters button.greek-option {
+            width: 100%;
+            min-height: 70px;
+        }
+    }
+</style>
--- src/utils/buildSentences.js
+++ src/utils/buildSentences.js
@@ -1,3 +1,54 @@
-export default function buildSentences(arrayWords) {
-    return;
+import selectRandomIndex from "./selectRandomIndex";
+
+export default function buildSentences(subjectArray, accusativeArray, verbs, predicativeArray) {
+    let sentence;
+    let traduction;
+    const randomSubjectIndex = selectRandomIndex(subjectArray);
+    const randomVerbIndex = selectRandomIndex(verbs);
+
+    const subject = subjectArray[randomSubjectIndex];
+    const verb = verbs[randomVerbIndex];
+
+    // Set a number that can be used as a probability to use first or third person.
+    const randomProbabilityToFirstPerson = generateRandomnessForThirdOrFirstPerson(10);
+
+    if (randomProbabilityToFirstPerson <= 2) { // 20% chance of being first person conjugate
+        sentence = addToSentence(sentence, `ἐγὼ ${verb.first}`);
+        traduction = addToSentence(traduction, `I ${`${verb.traduction === undefined ? verb.irregularFirst : `${verb.traduction}`}`}`);
+    } else {
+        sentence = addToSentence(sentence, `${subject.word} ${verb.third}`);
+        traduction = addToSentence(traduction, `The ${subject.traduction}  ${`${verb.traduction === undefined ? verb.irregularThird : `${verb.traduction}s`}`}`);
+    };
+
+    if (verb.accusative) {
+        const randomAccusativeIndex = selectRandomIndex(accusativeArray);
+        const accusative = accusativeArray[randomAccusativeIndex];
+
+        sentence = sentence = addToSentence(sentence, ` ${accusative.word}`);
+        traduction = addToSentence(traduction, ` ${accusative.traduction}`);
+    } else if (!verb.accusative && verb.predicative) {
+        const randomPredicativeIndex = selectRandomIndex(predicativeArray);
+        const predicative = predicativeArray[randomPredicativeIndex];
+
+        sentence = addToSentence(sentence, ` ${predicative[`${subject.gender}`]}`);
+        traduction = addToSentence(traduction, ` ${predicative.traduction}`);
+    };
+
+    return {
+        sentence,
+        traduction
+    };
+};
+
+function generateRandomnessForThirdOrFirstPerson(range) {
+    const randomNumber = Math.floor(Math.random() * range);
+    return randomNumber;
+};
+
+function addToSentence(sentenceToBeUpdated, update) {
+    if (sentenceToBeUpdated !== undefined) {
+        const newSentence = sentenceToBeUpdated + ` ${update}`;
+        return newSentence;
+    } else
+        return `${update}`;
 };
