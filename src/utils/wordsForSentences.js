39ff92f422e7f1a26be796e072621352162432ee
Emanuel Filipe <efilipecode@gmail.com>
2021-03-04 13:33:16 -0300

    Words for sentences

--- src/utils/wordsForSentences.js
+++ src/utils/wordsForSentences.js
@@ -1,4 +1,56 @@
-export const subject = {};
-export const accustive = {};
-export const verbs = {};
-// export const instrumentalDative = {};
+export const subject = [
+    { word: 'ὁ μαθητής', traduction: 'disciple', gender: 'masculine' },
+    { word: 'ὁ παῖς', traduction: 'kid', gender: 'masculine' },
+    { word: 'ὁ ἄνθρωπος', traduction: 'man', gender: 'masculine' },
+    { word: 'ἡ θύρα', traduction: 'gate', gender: 'feminine' },
+    { word: 'ἡ ἡδονή', traduction: 'pleasure', gender: 'feminine' },
+    { word: 'ἡ ψυχή', traduction: 'soul', gender: 'feminine' },
+    { word: 'ἡ μήτηρ', traduction: 'mother', gender: 'feminine' },
+    { word: 'ὁ ἀνήρ', traduction: 'man', gender: 'masculine' },
+    { word: 'ἡ ζωή', traduction: 'life', gender: 'feminine' },
+    { word: 'ὁ υἱός', traduction: 'son', gender: 'masculine' },
+    { word: 'ὁ θεός', traduction: 'goddess', gender: 'masculine' },
+];
+export const accustive = [
+    { word: 'τὸν ἄνδρα', traduction: 'the man' },
+    { word: 'τὸν υἱόν', traduction: 'the son' },
+    { word: 'τὸ πλοῖον', traduction: 'the ship' },
+    { word: 'τὸν νεκρόν', traduction: 'the corpse' },
+    { word: 'τὸ πράγμα', traduction: 'things' },
+    { word: 'τὸν θάνατον', traduction: 'death' },
+    { word: 'πάντα', traduction: 'everything' },
+    { word: 'τὸν ἥλιον', traduction: 'the sun' },
+    { word: 'τὸν βασιλέα', traduction: 'the king' },
+    { word: 'τὴν ἀλήθειαν', traduction: 'the truth' },
+    { word: 'τὴν ἀρετήv', traduction: 'the virtue' },
+    { word: 'τὸν λόγον', traduction: 'the word' },
+    { word: 'τὸν στρατηγόν', traduction: 'the general' },
+];
+
+export const verbs = [
+    { third: 'φιλεῖ', first: 'φίλῶ', traduction: 'like', accusative: true, predicative: false },
+    { third: 'ζητεῖ', first: 'ζητῶ', traduction: 'seek', accusative: true, predicative: false },
+    { third: 'ὁρᾶ', first: 'ὁρῶ', traduction: 'see', accusative: true, predicative: false },
+    { third: 'ἐστι', first: 'εἰμι', irregularFirst: 'am', irregularThird: 'is', accusative: false, predicative: true },
+    { third: 'κελεύει', first: 'κελεύω', traduction: 'command', accusative: false, predicative: false },
+    { third: 'τιμᾶ', first: 'τιμάω', traduction: 'honor', accusative: true, predicative: false },
+    { third: 'πάσχει', first: 'πάσχω', traduction: 'suffer', accusative: false, predicative: false },
+    { third: 'ἀποκρίνεται', first: 'ἀποκρίνομαι', traduction: 'answer', accusative: false, predicative: false },
+    { third: 'μανθάνει', first: 'μανθάνω', traduction: 'learn', accusative: false, predicative: false },
+    { third: 'δέχεται', first: 'δέχομαι', traduction: 'receive', accusative: true, predicative: false },
+    { third: 'φρονεῖ', first: 'φρονέω', traduction: 'think', accusative: false, predicative: false },
+    { third: 'μισεῖ', first: 'μισέω', traduction: 'hate', accusative: true, predicative: false },
+    { third: 'ἔχει', first: 'ἔχω', irregularFirst: 'have', irregularThird: 'has', accusative: true, predicative: false },
+    { third: 'παύει', first: 'παύω', traduction: 'stop', accusative: false, predicative: false },
+    { third: 'ὀλοφύρεται', first: 'ὀλοφύρομαι', traduction: 'lament', accusative: false, predicative: false },
+    { third: 'γίγνεται', first: 'γίγνομαι', traduction: 'become', accusative: false, predicative: true },
+];
+
+export const predicative = [
+    { masculine: 'ἀνδρεῖος', feminine: 'ἀνδρεῖα', neuter: 'ἀνδρεῖον', traduction: 'brave' },
+    { masculine: 'καλός', feminine: 'καλή', neuter: 'καλόν', traduction: 'beautiful' },
+    { masculine: 'κακός', feminine: 'κακή', neuter: 'κακόν', traduction: 'terrible' },
+    { masculine: 'θνητός', feminine: 'θνητή', neuter: 'θνητόν', traduction: 'mortal' },
+    // {masculine: '', feminine: '', neuter: ''},
+    // {masculine: '', feminine: '', neuter: ''},
+];
